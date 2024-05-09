4f75b097064dfaf7497d303d45fa7e06aafe66e5
Ayokunle Amodu <121697771+ayokunle321@users.noreply.github.com>
2024-05-09 12:35:21 -0600

    sorting out base cmakelist

---
+++ src/shell.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+#define MAX_INPUT_SIZE 1024
+#define MAX_ARGS 100
+
+void shell_loop() {
+	char input[MAX_INPUT_SIZE];
+    char *args[MAX_ARGS];
+}
+
